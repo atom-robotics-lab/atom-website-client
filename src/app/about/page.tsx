0f62e01326fef14c0dddb40382dc0ee1135196c0
gaurimadan <gaurimadan12@gmail.com>
2024-08-28 11:43:47 +0530

    navbar fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -123,7 +123,7 @@ const About = () => {
 
             {
               circleImageSrc: "/images/vansh.png",
-              name: "VANSH GUPTA (@gupta)",
+              name: "VANSH GUPTA (@vansh)",
               description: "CORE | CAD | IOT",
               instagramLink: "https://instagram.com/vanshgupta917",
               githubLink: "https://github.com/rule_keeper",

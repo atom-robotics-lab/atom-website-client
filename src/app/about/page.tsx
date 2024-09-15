b6ce6b771b1e01a2978023fc6a5b2e9aad097286
aasthaverma27 <aasthagunjan2@gmail.com>
2024-09-15 13:06:09 +0530

    added more members

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,21 +10,14 @@ const About = () => {
           <div className="bg-blue-600 text-white p-8 md:p-10 rounded-md text-center w-80 h-72 md:w-96 md:h-80 flex items-center justify-center mt-2 md:mt-6">
             <div>
               <p className="text-4xl font-bold mb-5">WE ARE A.T.O.M</p>
-              <p className="text-xl">A SOCIETY | COMMUNITY OF ROBOTICS BASED IN DELHI</p>
+              <p className="text-xl">A COMMUNITY OF ROBOTICS BASED IN DELHI</p>
             </div>
           </div>
         </div>
-        <div className="md:w-1/2 text-xl leading-relaxed mr-4 max-w-md md:max-w-lg">
+        {/* Further reduced the margin here */}
+        <div className="md:w-1/2 text-xl leading-relaxed mr-4 max-w-md md:max-w-lg mt-6 md:mt-8">
           <p>
-            Our world is heading towards an Industrial Revolution with the
-            introduction of Robotics & AI. Intelligent & Advanced robots are now
-            being used everywhere to either assist humans or execute tasks that
-            otherwise wouldn't have been possible by humans. Open source
-            technologies like Gazebo Robotics Simulator & ROS (Robot Operating
-            System) have made intelligent industrial/household robotics
-            accessible to students & researchers. As engineering students,
-            these technologies intrigue us to become a part of this industrial
-            revolution on the brink of innovation.
+            Welcome to A.T.O.M Lab, the robotics society of our college. At A.T.O.M, we focus on integrating software, hardware, and mechanical engineering to develop practical robotic solutions. Our projects include the MR Robot, an autonomous mobile robot, the Robotics ARM: A.J.G.A.R, and a people follower robot, among others. We are committed to advancing technology and providing students with hands-on experience in robotics.
           </p>
         </div>
       </div>
@@ -105,6 +98,14 @@ const About = () => {
               linkedinLink: "https://www.linkedin.com/in/aastha-verma-9459032aa/",
             },
             {
+              circleImageSrc: "/images/tanmay.jpg",
+              name: "TANMAY SINGH (@rubber_giant)",
+              description: "CORE | ROS",
+              instagramLink: "https://instagram.com/feels_on_wheels",
+              githubLink: "https://github.com/rubber-giant",
+              linkedinLink: "https://www.linkedin.com/in/tanmay-singh-07b539291?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+            },
+            {
               circleImageSrc: "/images/aryan.png",
               name: "ARYAN SETHI (@amplifier)",
               description: "CORE | ROS | CAD | IOT",
@@ -114,7 +115,7 @@ const About = () => {
             },
             {
               circleImageSrc: "/images/vansh.png",
-              name: "VANSH GUPTA (@vanshgupta3176)",
+              name: "VANSH GUPTA (@vansh)",
               description: "CORE | CAD | IOT",
               instagramLink: "https://instagram.com/vanshgupta917",
               githubLink: "https://github.com/rule_keeper",
@@ -129,44 +130,12 @@ const About = () => {
               linkedinLink: "https://www.linkedin.com/in/mehak-oberoi-9191a62b4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
             },
             {
-              circleImageSrc: "/images/deepansh.png",
-              name: "DEEPANSH GOEL (@bustin)",
-              description: "CORE | AI/ML | ROS",
-              instagramLink: "https://instagram.com/deepanshgl",
-              githubLink: "https://github.com/sudo-deep",
-              linkedinLink: "https://linkedin.com/in/deepanshgl",
-            },
-            {
-              circleImageSrc: "/images/akshar.png",
+              circleImageSrc: "/images/akshat.png",
               name: "AKSHAT (@hominum)",
               description: "CORE | Embedded Systems | IOT",
               instagramLink: "https://instagram.com/akshat_sharma05",
               githubLink: "https://github.com/AkshatSharma05",
               linkedinLink: "https://www.linkedin.com/in/akshat-sharma-166932161/",
-            },
-            {
-              circleImageSrc: "/images/tanmay.png",
-              name: "TANMAY SINGH (@rubber_giant)",
-              description: "CORE | ROS",
-              instagramLink: "https://instagram.com/feels_on_wheels",
-              githubLink: "https://github.com/rubber-giant",
-              linkedinLink: "https://www.linkedin.com/in/tanmay-singh-07b539291?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-            },
-            {
-              circleImageSrc: "/images/shlok.png",
-              name: "SHLOK SHARMA (@shawarma)",
-              description: "CORE | Embedded Systems | IOT",
-              instagramLink: "https://instagram.com/shlok_shawarma",
-              githubLink: "https://github.com/shloksharma273",
-              linkedinLink: "https://www.linkedin.com/in/shlok-sharma-b42320264/",
-            },
-            {
-              circleImageSrc: "/images/prakhar.png",
-              name: "PRAKHAR SHARMA (@pyarakhar)",
-              description: "CORE | Web Development",
-              instagramLink: "https://instagram.com/pyarakhar",
-              githubLink: "https://github.com/prakhar1535",
-              linkedinLink: "https://www.linkedin.com/in/prakhar1535/",
             },
             {
               circleImageSrc: "/images/mitanshu.png",
@@ -177,19 +146,96 @@ const About = () => {
               linkedinLink: "https://www.linkedin.com/in/mitanshu-goel-177624248/",
             },
             {
-              circleImageSrc: "/images/anurag.png",
-              name: "ANURAG (@augi)",
-              description: "CORE | ROS",
-              instagramLink: "https://instagram.com/anuragchoudhury.xz",
-              githubLink: " https://github.com/anurag-choudhury",
-              linkedinLink: " https://www.linkedin.com/in/anurag-choudhury-124b29259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-            }
+              circleImageSrc: "/images/deepansh.png",
+              name: "DEEPANSH GOEL (@bustin)",
+              description: "CORE | AI/ML | ROS",
+              instagramLink: "https://instagram.com/deepanshgl",
+              githubLink: "https://github.com/sudo-deep",
+              linkedinLink: "https://linkedin.com/in/deepanshgl",
+            },
+            {
+              circleImageSrc: "/images/prakhar.png",
+              name: "PRAKHAR SHARMA (@pyarakhar)",
+              description: "CORE | Embedded Systems | IOT",
+              instagramLink: "https://instagram.com/prakhar.sharma_27",
+              githubLink: "https://github.com/pyarakhar",
+              linkedinLink: "https://linkedin.com/in/prakhar-sharma27",
+            },
+              {
+                circleImageSrc: "/images/shlok.png",
+                name: "SHLOK SHARMA (@shawarma)",
+                description: "CORE | Embedded Systems | IOT",
+                instagramLink: "https://instagram.com/shlok_shawarma",
+                githubLink: "https://github.com/shloksharma273",
+                linkedinLink: "https://www.linkedin.com/in/shlok-sharma-b42320264/",
+              },
+  
+              {
+                circleImageSrc: "/images/anurag.png",
+                name: "ANURAG (@augi)",
+                description: "CORE | ROS",
+                instagramLink: "https://instagram.com/anuragchoudhury.xz",
+                githubLink: " https://github.com/anurag-choudhury",
+                linkedinLink: " https://www.linkedin.com/in/anurag-choudhury-124b29259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+              },
+              
+              {
+                circleImageSrc: "/images/pragya.png",
+                name: "PRAGYA JHA (@pragyajhaa_)",
+                description: "MAINTAINER | ROS",
+                instagramLink: "https://instagram.com/pragyajhaa_",
+                githubLink: " https://github.com/pragyajhaa",
+                linkedinLink: "https://www.linkedin.com/in/pragya-jha-849089293/",
+              },
+              {
+                circleImageSrc: "/images/mitul.png",
+                name: "MITUL (@mi16kool)",
+                description: "ELECTRONICS",
+                instagramLink: "https://instagram.com/_.wanderer.xo/",
+                githubLink: " https://github.com/mi-16kool",
+                linkedinLink: "https://www.linkedin.com/in/mitulsingh/",
+              },
+              {
+                circleImageSrc: "/images/kiriti.png",
+                name: "KIRITI (@kiriti8933)",
+                description: "ROS",
+                instagramLink: "https://www.instagram.com/k_.i_.t_.i_?igsh=MXVremUybndnZXAzNQ==",
+                githubLink: "https://github.com/kirinain",
+                linkedinLink: "https://www.linkedin.com/in/kiritinain",
+              },
+              {
+                circleImageSrc: "/images/rachit.png",
+                name: "RACHIT (@FlowerEmoji)",
+                description: "ROS",
+                instagramLink: "https://www.instagram.com/rachittt._04/",
+                githubLink: "https://github.com/Krachitkumar",
+                linkedinLink: "https://www.linkedin.com/in/kamarushirachitkumar/",
+              },
+              {
+                circleImageSrc: "/images/ritika.png",
+                name: "RITIKA (@ritika)",
+                description: "ROS | CAD",
+                instagramLink: "https://www.instagram.com/ritika_gupta0505/",
+                githubLink: "https://github.com/GuptaRitikaa",
+                linkedinLink: "https://www.linkedin.com/in/ritika-gupta-341736321?trk=contact-info",
+              },
 
-          ]}
-        />
+              {
+                circleImageSrc: "/images/ishaan.png",
+                name: "ISHAAN (@gene)",
+                description: "IOT",
+                instagramLink: "-",
+                githubLink: "https://github.com/Ishanb747",
+                linkedinLink: "https://www.linkedin.com/in/ishan-bhattacharya-032413275/",
+              },
+             
+            
+  
+            ]}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default About;
+    );
+  };
+  
+  export default About;
